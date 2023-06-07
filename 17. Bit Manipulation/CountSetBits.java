ed1e5c8989babde472104803f284f7604ceb68f7
Prince Kumar <princekumar70258@gmail.com>
2023-06-08 00:40:07 +0530

    "New Updates"

---
+++ 17. Bit Manipulation/CountSetBits.java
@@ -0,0 +1,15 @@
+public class CountSetBits {
+    public static int countSetBit(int n){
+        int count = 0;
+        while(n > 0){
+            if((n & 1) != 0){ // check our LSB
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countSetBit(15));
+    }
+}
