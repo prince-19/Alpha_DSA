786bc5203eeeb1a14870a27959e7d6bac29b9d1b
Prince Kumar <princekumar70258@gmail.com>
2023-08-12 00:41:09 +0530

    greedy_approach

---
+++ 28. Greedy Approach/Min_Absolute_difference_Pairs.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class Min_Absolute_difference_Pairs{
+    public static void main(String[] args) {  // T.C = O(nlogn) => because of sorting
+        // int A[] = {1, 2, 3};
+        // int B[] = {2, 1, 3};   // 0
+
+        int A[] = {4, 1, 8, 7};
+        int B[] = {2, 3, 6, 5};   // 6
+
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int minDiff = 0;
+
+        for(int i=0; i<A.length; i++){
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+
+        System.out.println("min absolute diff of pairs = " + minDiff);
+    }
+}
